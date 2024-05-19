4baf19e2a79a940aadbfeedb1ff5396e94bb406d
ElDanto <pavlovian.ch@gmail.com>
2024-05-19 20:45:09 +0300

    db seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,17 @@
 
 namespace Database\Seeders;
 
+use App\Models\Client;
+use App\Models\ClientStatus;
+use App\Models\Deal;
+use App\Models\DealStatus;
+use App\Models\RealEstateObject;
+use App\Models\RealEstateObjectStatus;
+use App\Models\RealEstateObjectType;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +21,24 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        ClientStatus::factory()->count(5)->create();
+        Client::factory()->count(20)->create();
+
+        DealStatus::factory()->count(5)->create();
+        $deals = Deal::factory()->count(15)->create();
+        
+        RealEstateObjectStatus::factory()->count(5)->create();
+        RealEstateObjectType::factory()->count(15)->create();
+        RealEstateObject::factory()->count(30)->create();
+
+
+        foreach ($deals as $deal) {
+            $clients = Client::find([rand(0, 20), rand(0, 20)]);
+            $deal->clients()->attach($clients);
+
+            $objects = RealEstateObject::find([rand(0, 30), rand(0, 30)]);
+            $deal->objects()->attach($objects);
+        }
     }
 }
