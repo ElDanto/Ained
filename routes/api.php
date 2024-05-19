11801b40a09f5ca3c15663c396e3751725bc4fab
ElDanto <pavlovian.ch@gmail.com>
2024-05-19 20:44:04 +0300

    add auth(Laravel Sanctum)

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,26 @@
 <?php
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ClientController;
+use App\Http\Controllers\DealController;
+use App\Http\Controllers\RealEstateObjectController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::get( '/objects', [RealEstateObjectController::class,'index'])->middleware('auth:sanctum');
+Route::get( '/objects/sortby/{params}', [RealEstateObjectController::class,'sort'])->middleware('auth:sanctum');
+Route::get('/objects/{id}', [RealEstateObjectController::class,'show'])->middleware('auth:sanctum');
+
+Route::get('/clients', [ClientController::class, 'index'])->middleware('auth:sanctum');
+Route::get('/clients/{id}', [ClientController::class, 'show'])->middleware('auth:sanctum');
+
+Route::get('/deals', [DealController::class,'index'])->middleware('auth:sanctum');
+Route::get('/deals/{id}', [DealController::class,'show'])->middleware('auth:sanctum');
+
